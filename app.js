aafb6fed7c52c277a8d2c7d0b31755715e7f4a3a
yohanes <yohanesaditia1312@gmail.com>
2024-07-03 09:52:33 +0800

    -d

--- app.js
+++ app.js
@@ -15,12 +15,23 @@ var io = require("socket.io")(server, {
   },
 });
 
-function conectWA(socket, id) {
+function conectWA(socket) {
+  var id = 990873;
   const client = new Client({
+    takeoverOnConflict: true,
+    takeoverTimeoutMs: 0,
+
     authStrategy: new LocalAuth({
       // dataPath: folder,
       clientId: id,
     }),
+
+    webVersionCache: {
+      type: "remote",
+      remotePath:
+        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    },
+
     puppeteer: {
       headless: true,
       args: [
@@ -32,7 +43,7 @@ function conectWA(socket, id) {
         "--no-zygote",
         "--disable-gpu",
       ],
-      timeout: 30000,
+      //   timeout: 30000,
     },
   });
   let inc = 0;
@@ -54,6 +65,7 @@ function conectWA(socket, id) {
   });
   client.on("disconnected", (reason) => {
     console.log("disconnect Whatsapp-bot", reason);
+    client.initialize();
   });
   clients[id] = client;
   client.initialize();
@@ -62,7 +74,7 @@ function conectWA(socket, id) {
 io.on("connection", (socket) => {
   console.log("connected-socket" + port);
   socket.on("qr", (id) => {
-    conectWA(socket, id);
+    conectWA(socket);
   });
 });
 
@@ -72,14 +84,17 @@ app.post("/api/send", async (req, res) => {
   const phone = req.body.phone;
   const message = req.body.message;
   const client_id = req.body.client_id;
-  const media = await MessageMedia.fromUrl(
-    "https://via.placeholder.com/350x150.png"
-  );
+  //   const media = await MessageMedia.fromUrl(
+  //     "https://via.placeholder.com/350x150.png"
+  //   );
 
   clients[client_id]
-    .sendMessage(phone.substring(1) + "@c.us", media, {
-      caption: message,
-    })
+    .sendMessage(phone.substring(1) + "@c.us", message)
+
+    // .sendMessage(phone.substring(1) + "@c.us", media, {
+    //   caption: message,
+    // })
+
     .then((response) => {
       res.status(200).json({
         error: false,
